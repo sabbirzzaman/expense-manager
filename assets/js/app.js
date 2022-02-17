aa53cb26ed7948f655fc7a85c189225827497ade
sabbirzzaman <sztonmoy7@gamil.com>
2022-02-17 12:51:15 +0600

    mobile resopnsive done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,23 @@
-console.log('haha')
+const expensesBtn = document.getElementById('expenses-btn');
+
+function inputAmount(inputId) {
+    const amountElement = document.getElementById(inputId);
+    const totalAmountText = amountElement.value;
+    const totalAmount = parseFloat(totalAmountText);
+
+    amountElement.value = '';
+    return totalAmount;
+}
+
+expensesBtn.addEventListener('click', function() {
+    const totalExpenses = document.getElementById('total-amount');
+
+    const foodExpenses = inputAmount('food');
+    const rentExpenses = inputAmount('rent');
+    const clothesExpenses = inputAmount('clothes');
+
+    const newTotalExpenses = foodExpenses + rentExpenses + clothesExpenses;
+    totalExpenses.innerText = newTotalExpenses;
+
+    console.log(newTotalExpenses)
+})
